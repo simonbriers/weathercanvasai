9ea2c80d11291bf1ed487830e846d29ec453528b
simonbriers <simon.briers@gmail.com>
2023-11-20 18:22:37 +0100

    changes up to evenint 20/11/23

--- __init__.py
+++ __init__.py
@@ -1,17 +1,30 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN
+import logging
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.helpers import entity_platform
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "weather_image_generator"
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the weather_image_generator component."""
-    # Placeholder for your component setup logic
+    # Register your service
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(
+        "weather2img",
+        {},
+        "handle_weather2img"
+    )
+
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up weather_image_generator from a config entry."""
-    # Placeholder for setting up a config entry
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-    # Load your platform, if applicable
-    # hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
-    return True
+async def handle_weather2img(service_call: ServiceCall):
+    # This function will handle the service call
+    # Placeholder for calling your image generation logic
+    # Example: await image_generator.generate_image()
+
+    # Logging the service call for debugging
+    _LOGGER.info("weather2img service called")
+
+    # Implement the functionality or call to the separate module here
+    # ...
+
