60da856f300e0aeaab5d1a1a02fed871c76b8508
simonbriers <simon.briers@gmail.com>
2023-12-07 08:29:11 +0100

    deleted loggin from sensor.py

--- custom_components/weathercanvasai/sensor.py
+++ custom_components/weathercanvasai/sensor.py
@@ -3,21 +3,28 @@ from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.const import ATTR_ATTRIBUTION
 from .const import DOMAIN
 from datetime import datetime
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 class weathercanvasaiPromptsSensor(SensorEntity):
     def __init__(self, hass, entry_id, name):
+        #_LOGGER.info("Initializing weathercanvasaiPromptSensor")
         """Initialize the sensor."""
         self.hass = hass
         self.entry_id = entry_id
         self._attr_name = name
-        self._attr_unique_id = entry_id  # Use entry_id as the unique ID
         self._state = "Initial State"  # Default state
+        self._attr_unique_id = f"{entry_id}_prompts"
+
         self._attributes = {
             "chatgpt_in": "Initial chatgpt_in",
             "chatgpt_out": "Initial chatgpt_out",
             "last_update": datetime.now().isoformat()
         }
         self._attr_icon = 'mdi:chat'  # Set the icon here
+        #_LOGGER.info(f"Initializing weathercanvasaiPeomptsSensor with unique ID: {self._attr_unique_id}")
+
 
     @property
     def name(self):
@@ -51,7 +58,37 @@ class weathercanvasaiPromptsSensor(SensorEntity):
         self._attributes["last_update"] = datetime.now().isoformat()
         self.async_write_ha_state()
 
+class weathercanvasaiImageSensor(SensorEntity):
+    def __init__(self, hass, entry_id, name):
+        #_LOGGER.info("Initializing weathercanvasaiImageSensor")
+        """Initialize the image URL sensor."""
+        self.hass = hass
+        self.entry_id = entry_id
+        self._attr_name = name
+        self._attr_unique_id = f"{entry_id}_image"  # Example unique ID
+        #_LOGGER.info(f"Initializing weathercanvasaiImageSensor with unique ID: {self._attr_unique_id}")
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._attr_name
+
+    @property
+    def state(self):
+        """Return the state of the sensor (URL of the latest image)."""
+        return self._state
+
+    async def async_added_to_hass(self):
+        """Register callbacks when entity is added."""
+        self._state = self.hass.data[DOMAIN].get('latest_image_url')
+
+    async def _update_sensor(self):
+        """Update the sensor state (image URL)."""
+        self._state = self.hass.data[DOMAIN].get('latest_image_url')
+        self.async_write_ha_state()
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the sensor upon entry setup."""
-    sensor = weathercanvasaiPromptsSensor(hass, config_entry.entry_id, "weathercanvasai Prompts")
-    async_add_entities([sensor])
+    """Set up the sensors upon entry setup."""
+    prompts_sensor = weathercanvasaiPromptsSensor(hass, config_entry.entry_id, "weathercanvasai Prompts")
+    image_sensor = weathercanvasaiImageSensor(hass, config_entry.entry_id, "weathercanvasai Image") 
+    async_add_entities([prompts_sensor, image_sensor])
