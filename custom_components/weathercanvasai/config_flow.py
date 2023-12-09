f58658a278256613831d89138bcb1bb18274ce1f
simonbriers <simon.briers@gmail.com>
2023-12-09 15:16:48 +0100

    Updated config_flow

--- custom_components/weathercanvasai/config_flow.py
+++ custom_components/weathercanvasai/config_flow.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Tuple
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
@@ -9,12 +10,29 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 STEP_USER_DATA_SCHEMA = vol.Schema({
-    vol.Required('openai_api_key', default="your openai api key here"): str,
-    vol.Required('googlemaps_api_key', default="your googlemaps api key here"): str,
+    vol.Required('openai_api_key', default= None): str,
+    vol.Required('googlemaps_api_key', default= None): str,
     vol.Required('gpt_model_name', default='gpt-3.5-turbo'): vol.In(['gpt-3.5-turbo', 'gpt-4']),
     vol.Required('max_images_retained', default=5): int,
     vol.Required('system_instruction', default="Create a succinct DALL-E prompt under 100 words, that will create an artistic image, focusing on the most visually striking aspects of the given city/region, weather, and time of day. Highlight key elements that define the scene's character, such as specific landmarks, weather effects, folklore or cultural features, in a direct and vivid manner. Avoid elaborate descriptions; instead, aim for a prompt that vividly captures the essence of the scene in a concise format, suitable for generating a distinct and compelling image."): str
 })
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> Tuple[bool, str, str, str]:
+    openai_api_key = data['openai_api_key']
+    googlemaps_api_key = data['googlemaps_api_key']
+
+    # Test the OpenAI API key
+    openai_test_success, openai_error = await test_openai_api(openai_api_key)
+    if not openai_test_success:
+        return False, openai_error or 'openai_api_test_fail', '', None
+
+    # Test the Google Maps API key
+    googlemaps_test_success, googlemaps_error, google_location_name = await test_googlemaps_api(hass, googlemaps_api_key)
+    if not googlemaps_test_success:
+        return False, '', googlemaps_error or 'googlemaps_api_test_fail', None
+
+    # If both tests pass, return success along with the location name
+    return True, '', '', google_location_name
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -37,17 +55,20 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_show_form(step_id="user", errors=errors)
 
         if user_input is not None:
-            # Test the OpenAI API key
-            openai_test_success, openai_error = await test_openai_api(user_input['openai_api_key'])
-            if not openai_test_success:
-                errors['openai_api_key'] = openai_error or 'openai_api_test_fail'
+            success, openai_error, googlemaps_error, google_location_name = await validate_input(self.hass, user_input)
 
-            # Test the Google Maps API key and retrieve location name
-            googlemaps_test_success, googlemaps_error, google_location_name = await test_googlemaps_api(self.hass, user_input['googlemaps_api_key'])
-            if not googlemaps_test_success:
-                errors['googlemaps_api_key'] = googlemaps_error or 'googlemaps_api_test_fail'
+            if not success:
+                # Add specific errors for each API key if validation fails
+                if openai_error:
+                    errors['openai_api_key'] = openai_error
+                if googlemaps_error:
+                    errors['googlemaps_api_key'] = googlemaps_error
 
-            if not errors:
+                # Redisplay the form with errors
+                return self.async_show_form(
+                    step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+                )
+            else:
                 # Store the valid API keys, model choices, and location name temporarily
                 self.openai_api_key = user_input['openai_api_key']
                 self.googlemaps_api_key = user_input['googlemaps_api_key']
